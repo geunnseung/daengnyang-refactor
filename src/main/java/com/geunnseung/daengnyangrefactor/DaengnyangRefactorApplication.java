8924485168342364fbdab0bbc7a365941a01c900
geunnseung <bsg1017@gmail.com>
2026-02-04 23:04:15 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/geunnseung/daengnyangrefactor/DaengnyangRefactorApplication.java
@@ -0,0 +1,13 @@
+package com.geunnseung.daengnyangrefactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaengnyangRefactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaengnyangRefactorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geunnseung/daengnyangrefactor/DaengnyangRefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geunnseung.daengnyangrefactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaengnyangRefactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
