a85ef7b02fa0bac86b221c5d7b890cdf04ae5f5d
geunnseung <bsg1017@gmail.com>
2026-05-04 23:31:55 +0900

    refactor: PetService 권한 검증 및 요청 처리 개선

---
+++ src/main/java/com/geunnseung/daengnyangrefactor/pet/service/command/PetGroupCreateCommand.java
@@ -0,0 +1,8 @@
+package com.geunnseung.daengnyangrefactor.pet.service.command;
+
+public record PetGroupCreateComman(
+
+        String name,
+        String description
+) {
+}
---
+++ src/main/java/com/geunnseung/daengnyangrefactor/pet/service/command/PetRegisterCommand.java
@@ -0,0 +1,4 @@
+package com.geunnseung.daengnyangrefactor.pet.service.command;
+
+public record PetRegisterCommand() {
+}
